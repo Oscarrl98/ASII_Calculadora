aadc5294c6cf67bdbc3fc78ebb04e071a3bdf053
greyes97 <gustadolfo.rl@outlook.com>
2020-07-23 21:13:59 -0600

    Se ha creado la interfaz.

---
+++ ICalculadora.java
@@ -0,0 +1,8 @@
+public interface ICalculadora{
+	public int suma(int a,int b);
+	public int resta(int a,int b);
+	public int multiplicacion(int a,int b);
+	public int division(int a,int b);
+	public int modulo(int a,int b);
+	public int factorial(int a,int b);
+}
